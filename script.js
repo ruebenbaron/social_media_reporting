a4a229dedf2e7925c171b74a624ce595d2da22e8
ruebenbaron <ruebenbaronvomacker@gmail.com>
2016-11-16 18:51:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,9 @@ $(document).ready(function(){
   }
   
   function numberWithDots(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    var parts = x.toString().split(",");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return parts.join(".");
   }
   
   function createTable(container, wettbewerber, kriterien) {

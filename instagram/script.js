523138044ffbb70025c3f2767b8ae45057b8d390
ruebenbaron <ruebenbaronvomacker@gmail.com>
2016-11-18 11:11:59 +0100

    Update script.js

--- instagram/script.js
+++ instagram/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   var url = window.location.href;
   var code_index = url.indexOf("code");
   if (code_index > 0) {
-    var code = url.substr(code_index,url.length);
+    var code = url.substr(code_index + 5,url.length);
     console.log(code);
   };
 });

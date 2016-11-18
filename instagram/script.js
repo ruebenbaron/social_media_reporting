8d086e6c0a6c70dae0db380f8884164f53f86136
ruebenbaron <ruebenbaronvomacker@gmail.com>
2016-11-18 11:44:51 +0100

    Update script.js

--- instagram/script.js
+++ instagram/script.js
@@ -1,8 +1,64 @@
 $(document).ready(function(){
+  
+  //Functions:
+  function createTable(container_id, wettbewerber, kriterien) {
+    var containerTable = document.getElementById(container);
+    var table = document.createElement("table");
+    table.id = "dashboard";
+    table.className = "table";
+    table.className += " table-striped table-hover";
+    containerTable.appendChild(table);
+
+    var tableData = {};
+    tableData.header = {};
+    for (i=0; i<wettbewerber.length; i++) {
+      tableData[wettbewerber[i]] = {};
+    };
+
+
+    for (i=0; i<=wettbewerber.length; i++) {
+      var tr = document.createElement("tr");
+      tr.id = i;
+      table.appendChild(tr);
+
+      for (x=0; x<kriterien.length; x++) {
+        switch (i) {
+          case 0:
+            var th = document.createElement("th");
+            th.id = kriterien[x];
+            th.innerHTML = kriterien[x];
+            tr.appendChild(th);
+            tableData.header[kriterien[x]] = th;
+            break;
+          default:
+            var td = document.createElement("td");
+            td.id = wettbewerber[i-1] + "_" + kriterien[x];
+            td.className = wettbewerber[i-1] + " " + kriterien[x];
+            if(x>0){
+              td.className += " number";
+            }
+            //td.innerHTML = wettbewerber[i-1] + " " + kriterien[x];
+            tr.appendChild(td);
+            tableData[wettbewerber[i-1]][kriterien[x]] = td;
+            break;
+        }
+      };
+    };
+    
+    return tableData;
+  }
+  
+  //Instagram API Init:
   var url = window.location.href;
   var token_index = url.indexOf("token");
   if (token_index > 0) {
     var access_token = url.substr(token_index + 6,url.length);
-    console.log(access_token);
+    
+    //Analyse-Daten:
+    var wettbewerber = ["vwfsde","mercedesbenzbank","ingdiba", "targobank", "fidorde", "consorsbank", "commerzbank", "barclaysUK", "citibank", "chase"];
+    var kriterien = ["Page", "Fans", "Posts_Count", "Avg_Likes_per_Post", "Most_Successful_Post_Likes", "Avg_Engagement_Rate_per_Post"];
+    
+    //Create Table:
+    var table = createTable("containerTable", wettbewerber, kriterien);
   };
 });

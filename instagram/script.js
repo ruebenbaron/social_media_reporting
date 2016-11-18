ed769c4827978aa4bdacb54a04bc7a08660d0c5f
ruebenbaron <ruebenbaronvomacker@gmail.com>
2016-11-18 12:23:10 +0100

    Add Login-Status Update

--- instagram/script.js
+++ instagram/script.js
@@ -68,6 +68,7 @@ $(document).ready(function(){
   var url = window.location.href;
   var token_index = url.indexOf("token");
   if (token_index > 0) {
+    $("#login-status").text("Logged in.");
     var access_token = url.substr(token_index + 6,url.length);
     for (i=0; i < wettbewerber.length; i++) {
       fillFollower_Count(wettbewerber[i], access_token, tableData);

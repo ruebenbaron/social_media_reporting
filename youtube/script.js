08d8e2f6775f2d0e3f7fbc5fbb9d4adc18d067fa
ruebenbaron <ruebenbaronvomacker@gmail.com>
2016-12-05 13:35:50 +0100

    Add: New Input already exists handler

--- youtube/script.js
+++ youtube/script.js
@@ -530,59 +530,64 @@ $(document).ready(function(){
   $(document).on("keyup", "#competitor_input", function(event){
   //$("#competitor_input").keyup(function(event){
     if(event.which == 13){
-      console.log("Enter was pressed.");
-      //Change td.ids to last wettbewerber
-      for (x=0; x<kriterien.length; x++) {
-        var td = tableData["new_competitor"][kriterien[x]]
-        //Set Input Text As last wettbewerber:
-        var new_input = $("#competitor_input").val()
-        wettbewerber[wettbewerber.length-1] = new_input
-        td.id = new_input + "_" + kriterien[x];
-        td.className = new_input + " " + kriterien[x];
-        if(x>0){
-          td.className += " number";
+      var new_input = $("#competitor_input").val()
+      if (tableData.hasOwnProperty(new_input)){
+        //If User enters existing Channel Name:
+        $("#competitor_input").blur();
+        alert("Dieser Channel ist in der Tabelle bereits enthalten.");
+      } else {
+        //If User enters new Channel Name:
+        //Change td.ids to last wettbewerber
+        for (x=0; x<kriterien.length; x++) {
+          var td = tableData["new_competitor"][kriterien[x]]
+          //Set Input Text As last wettbewerber:
+          wettbewerber[wettbewerber.length-1] = new_input
+          td.id = new_input + "_" + kriterien[x];
+          td.className = new_input + " " + kriterien[x];
+          if(x>0){
+            td.className += " number";
+          }
         }
-      }
-      //Correct tableData Object:
-      if (tableData.hasOwnProperty(new_input) == false){
+        //Correct tableData Object:
         tableData[new_input] = tableData["new_competitor"];
         delete tableData["new_competitor"]
-      }
-      //Call YouTube Functions for last wettbewerber
-      appendPageDiv(wettbewerber[wettbewerber.length-1]);
-      fillChannelName(wettbewerber[wettbewerber.length-1], tableData);
-      fillSubscriptions(wettbewerber[wettbewerber.length-1], tableData);
-      fillVideos_Count(wettbewerber[wettbewerber.length-1], tableData);
-      fillAvg_Views_per_Video(wettbewerber[wettbewerber.length-1], tableData);
-      fillAvg_Likes_per_Video(wettbewerber[wettbewerber.length-1], tableData);
-      fillMost_Successful_Video_Views(wettbewerber[wettbewerber.length-1], tableData);
-      fillMost_Successful_Video_Likes(wettbewerber[wettbewerber.length-1], tableData);
-      fillAvg_Views_compared_to_Subs(wettbewerber[wettbewerber.length-1], tableData);
-      fillAvg_Engagement_Rate_per_Video(wettbewerber[wettbewerber.length-1], tableData);
-      //Add "new_competitor" to wettbewerber
-      var new_arr_element = "new_competitor"
-      wettbewerber.push(new_arr_element);
-      //Update tableData Object:
-      tableData[new_arr_element] = {};
-      //Add tr with input field to table
-      var table = document.getElementById("dashboard");
-      var tr = document.createElement("tr")
-      for (x=0; x<kriterien.length; x++){
-        var td = document.createElement("td");
-        td.id = new_arr_element + "_" + kriterien[x];
-        td.className = new_arr_element + " " + kriterien[x];
-        if(x>0){
-          td.className += " number";
-        } else {
-          var input = document.createElement("input");
-          input.id = "competitor_input";
-          input.setAttribute("type", "text");
-          td.appendChild(input);
+        //Call YouTube Functions for last wettbewerber
+        appendPageDiv(wettbewerber[wettbewerber.length-1]);
+        fillChannelName(wettbewerber[wettbewerber.length-1], tableData);
+        fillSubscriptions(wettbewerber[wettbewerber.length-1], tableData);
+        fillVideos_Count(wettbewerber[wettbewerber.length-1], tableData);
+        fillAvg_Views_per_Video(wettbewerber[wettbewerber.length-1], tableData);
+        fillAvg_Likes_per_Video(wettbewerber[wettbewerber.length-1], tableData);
+        fillMost_Successful_Video_Views(wettbewerber[wettbewerber.length-1], tableData);
+        fillMost_Successful_Video_Likes(wettbewerber[wettbewerber.length-1], tableData);
+        fillAvg_Views_compared_to_Subs(wettbewerber[wettbewerber.length-1], tableData);
+        fillAvg_Engagement_Rate_per_Video(wettbewerber[wettbewerber.length-1], tableData);
+        //Add "new_competitor" to wettbewerber
+        var new_arr_element = "new_competitor"
+        wettbewerber.push(new_arr_element);
+        //Update tableData Object:
+        tableData[new_arr_element] = {};
+        //Add tr with input field to table
+        var table = document.getElementById("dashboard");
+        var tr = document.createElement("tr")
+        for (x=0; x<kriterien.length; x++){
+          var td = document.createElement("td");
+          td.id = new_arr_element + "_" + kriterien[x];
+          td.className = new_arr_element + " " + kriterien[x];
+          if(x>0){
+            td.className += " number";
+          } else {
+            var input = document.createElement("input");
+            input.id = "competitor_input";
+            input.setAttribute("type", "text");
+            td.appendChild(input);
+          }
+          tr.appendChild(td);
+          tableData[new_arr_element][kriterien[x]] = td;
         }
-        tr.appendChild(td);
-        tableData[new_arr_element][kriterien[x]] = td;
+        table.appendChild(tr);
+        input.focus();
       }
-      table.appendChild(tr);
     }
   });
 });

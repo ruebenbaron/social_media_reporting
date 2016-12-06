bebed3f08077a805d1c193b9cbcf5436054a353b
ruebenbaron <ruebenbaronvomacker@gmail.com>
2016-12-06 15:02:07 +0100

    Bug Fix: FB.XFBML.parse()

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ $(document).ready(function(){
     divPost.setAttribute("data-href", post_permalink_url);
     divPage.appendChild(divPost);
     //Parse FB Elements again
-    FB.XFBML.parse(divPost);
+    FB.XFBML.parse();
   }
 
   function appendPostInfo(page_name, post_id) {
